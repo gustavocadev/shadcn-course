47cdb13ce180d0cc385c282053a09499b5e1c642
gustavocadev <gustavocadev@gmail.com>
2024-05-29 20:18:31 -0500

    Add dashboard

---
+++ app/routes/dashboard.accordion.tsx
@@ -0,0 +1,7 @@
+export default function DashboardAccordion() {
+  return (
+    <>
+      <h1>Accordion Page!</h1>
+    </>
+  );
+}
---
+++ app/routes/dashboard.alert.tsx
@@ -0,0 +1,7 @@
+export default function DashboardAlert() {
+  return (
+    <>
+      <h1>Alert Page!</h1>
+    </>
+  );
+}

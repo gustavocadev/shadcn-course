4be44e2010155f3f52e9f0ae6587af30fbf61299
gustavocadev <gustavocadev@gmail.com>
2024-06-08 18:52:32 -0500

    Add components

--- app/routes/dashboard.card.tsx
+++ app/routes/dashboard.card.tsx
@@ -7,10 +7,12 @@ import {
   CardHeader,
   CardTitle,
 } from '~/components/ui/card';
+import { Input } from '~/components/ui/input';
+import { Label } from '~/components/ui/label';
 
 export default function CardPage() {
   return (
-    <div className="grid grid-cols-3 gap-4">
+    <div className="grid grid-cols-1 sm:grid-cols-3 gap-4">
       {Array.from({ length: 9 }).map((_, idx) => (
         <Card key={idx}>
           <CardHeader>
@@ -26,6 +28,29 @@ export default function CardPage() {
           </CardFooter>
         </Card>
       ))}
+
+      <Card className="cols-span-1 sm:col-span-3">
+        <CardHeader>
+          <CardTitle>Create project</CardTitle>
+          <CardDescription>
+            Deploy your new project in one-click.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="grid w-full items-center gap-4">
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="name">Name</Label>
+                <Input id="name" placeholder="Name of your project" />
+              </div>
+            </div>
+          </form>
+        </CardContent>
+        <CardFooter className="flex justify-between">
+          <Button variant="outline">Cancel</Button>
+          <Button>Deploy</Button>
+        </CardFooter>
+      </Card>
     </div>
   );
 }
---
+++ app/routes/dashboard.carousel.tsx
@@ -0,0 +1,39 @@
+import { Card, CardContent } from '~/components/ui/card';
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from '~/components/ui/carousel';
+
+export default function CarouselPage() {
+  return (
+    <div className="w-full flex justify-center">
+      <Carousel
+        className="w-full max-w-xs"
+        opts={{
+          // dragFree: true,
+          loop: true,
+        }}
+        // autoPlay={1000}
+      >
+        <CarouselContent>
+          {Array.from({ length: 5 }).map((_, index) => (
+            <CarouselItem key={index} className="md:basis-1/2">
+              <div className="p-1">
+                <Card>
+                  <CardContent className="flex aspect-square items-center justify-center p-6">
+                    <span className="text-4xl font-semibold">{index + 1}</span>
+                  </CardContent>
+                </Card>
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious className="hidden sm:flex" />
+        <CarouselNext className="hidden sm:flex" />
+      </Carousel>
+    </div>
+  );
+}

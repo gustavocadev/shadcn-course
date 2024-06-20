23b4c125d4dd968f819e5cfac261e6277b8fe789
gustavocadev <gustavocadev@gmail.com>
2024-06-20 10:47:11 -0500

    Add data table component

--- app/routes/dashboard.data-table/route.tsx
+++ app/routes/dashboard.data-table/route.tsx
@@ -1,16 +1,18 @@
 import type { LoaderFunctionArgs } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import { payments } from "~/data/payments.data";
+import { columns } from "./columns";
+import { DataTable } from "./data-table";
 
 export async function loader({ request }: LoaderFunctionArgs) {
   return payments;
 }
 
-export default function DataTable() {
+export default function DataTablePage() {
   const data = useLoaderData<typeof loader>();
   return (
     <>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
+      <DataTable columns={columns} data={data} />
     </>
   );
 }
